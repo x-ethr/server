2dcdf29589674cbeb75b807bfba63093cf3d7794
Jacob B. Sanders <segmentational@gmail.com>
2024-05-30 04:34:10 -0600

    Handler Module

--- handler/channels.go
+++ handler/channels.go
@@ -1,6 +1,6 @@
 package handler
 
-func Channels[Input interface{}, Body interface{}, Output interface{}](input chan *Input, body chan *Body, output chan *Output, exception chan *Exception, invalid chan *Invalid) {
+func Channels[Input interface{}, Body interface{}, Output interface{}]() (input chan *Input, body chan *Body, output chan *Output, exception chan *Exception, invalid chan *Invalid) {
 	input, body, output, exception, invalid = make(chan *Input), make(chan *Body, 1), make(chan *Output), make(chan *Exception), make(chan *Invalid)
 
 	return
